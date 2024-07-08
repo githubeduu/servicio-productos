f369ee055d078d32126e5e5c3057ef518a890370
Eduardo Carvajal <edo.carvajal@hotmail.com>
2024-07-08 09:11:28 -0400

    nuevo build

--- src/main/java/com/example/servicio_productos/config/CorsConfig.java
+++ src/main/java/com/example/servicio_productos/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Aquí debes poner la URL de tu aplicación Angular
+                .allowedOrigins("*") // Aquí debes poner la URL de tu aplicación Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
